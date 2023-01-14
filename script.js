d258d9f961d0d16e603e6c1b810cb96163dbc4bb
Glitch (likeable-decisive-bismuth) <none>
2023-01-14 12:03:56 +0000

    左上空白を右下空白に変更

--- script.js
+++ script.js
@@ -14,24 +14,11 @@ function init() {
       const index = i * 4 + j;
       td.className = "tile";
       td.index = index;
-      if (true) {
-        // 空白が左上
-        td.value = index;
-        if (index == 0) {
-          td.textContent = "";
-        } else {
-          td.textContent = index;
-        }
+      if (index < 15) {
+        td.textContent = index + 1;
       } else {
-        // 空白が右下
-        td.value = index + 1;
-        if (index < 15) {
-          td.textContent = index + 1;
-        } else {
-          td.textContent = "";
-        }
+        td.textContent = "";
       }
-
       td.onclick = click;
       tr.appendChild(td);
       tiles.push(td);
@@ -40,31 +27,29 @@ function init() {
   }
 
   for (let i = 0; i < 1000; i++) {
-    //click({ target: { index: Math.floor(Math.random() * 16) } });
+    click({ target: { index: Math.floor(Math.random() * 16) } });
   }
 }
 
 function click(e) {
   const i = e.target.index;
-  if (i - 4 >= 0 && tiles[i - 4].value == 0) {
+  if (i - 4 >= 0 && tiles[i - 4].textContent == "") {
     // クリックしたマスの上が空いている場合、上と入れ替え
     swap(i, i - 4);
-  } else if (i + 4 < 16 && tiles[i + 4].value == 0) {
+  } else if (i + 4 < 16 && tiles[i + 4].textContent == "") {
     // クリックしたマスの下が空いている場合、下と入れ替え
     swap(i, i + 4);
-  } else if (i % 4 != 0 && tiles[i - 1].value == 0) {
+  } else if (i % 4 != 0 && tiles[i - 1].textContent == "") {
     // クリックしたマスの左が空いている場合、左と入れ替え
     swap(i, i - 1);
-  } else if (i % 4 != 3 && tiles[i + 1].value == 0) {
+  } else if (i % 4 != 3 && tiles[i + 1].textContent == "") {
     // クリックしたマスの右が空いている場合、右と入れ替え
     swap(i, i + 1);
   }
 }
 
 function swap(i, j) {
-  const tmp = tiles[i].value;
+  const tmp = tiles[i].textContent;
   tiles[i].textContent = tiles[j].textContent;
-  tiles[i].value = tiles[j].value;
   tiles[j].textContent = tmp;
-  tiles[j].value = tmp;
 }
